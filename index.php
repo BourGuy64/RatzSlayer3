d7f56bd8391b469beda32706a9850be7cd54ce5e
ctrlouis <ctrlouis@c55ef46ffb76>
2019-09-10 09:49:47 +0000

    add twig

--- index.php
+++ index.php
@@ -10,9 +10,36 @@ require 'vendor/autoload.php';
  */
 $app = new \Slim\App;
 
+
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('src/views', [
+        // 'cache' => 'path/to/cache'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
+};
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->view->render($response, 'header.html.twig', []);
+});
+
 /**
  * test
  */
+ $app->get('/cake', function (Request $req, Response $res, array $args) {
+     $res->getBody()->write("Cake is a lie");
+     return $res;
+ });
+
 $app->get('/', function (Request $req, Response $res, array $args) {
     $res->getBody()->write("Coffe is ready!");
     return $res;
