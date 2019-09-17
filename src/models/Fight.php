3625ca9b3b55ce25e65a85aa4bb1b264a0f03233
Victor Bour <victor.bour.bowden@gmail.com>
2019-09-17 09:43:56 +0200

    -Add join between Fight and FightLog

--- src/models/Fight.php
+++ src/models/Fight.php
@@ -7,5 +7,9 @@ class Fight extends \Illuminate\Database\Eloquent\Model
     protected $table = 'fight';
     protected $primaryKey = 'id';
     public $timestamps = false;
+
+    public function fightlog() {
+        return $this->hasMany('ratzslayer3\models\FightLog', 'id_fight');
+    }
 }
 ?>
--- src/models/FightLog.php
+++ src/models/FightLog.php
@@ -7,6 +7,10 @@ class FightLog extends \Illuminate\Database\Eloquent\Model
     protected $table = 'fight_log';
     protected $primaryKey = 'id';
     public $timestamps = false;
+
+    public function fightlog() {
+      return $this->belongsTo('ratzslayer3\models\Fight' , 'id_fight');
+    }
 }
 
 ?>
