a958b83cdebce70ca5faa6538734dfee1061f90d
ctrlouis <louis.bty57@gmail.com>
2019-09-24 18:49:15 +0000

    add function for create new monster

--- src/controllers/MonstersController.php
+++ src/controllers/MonstersController.php
@@ -15,4 +15,25 @@ class MonstersController extends SuperController {
     public function createForm(Request $req, Response $res, array $args) {
         return $this->views->render($res, 'form-monster.html.twig', ['title' => 'New character', 'dir' =>  $this->dir]);
     }
+
+    public function create(Request $req, Response $res, array $args) {
+        $body = json_decode($req->getBody());
+
+        $monster = MST::where('name', 'like', $body->name)->first();
+        if ($monster) {
+            return $res->withStatus(400);
+        }
+
+        $monster = new MST;
+        $monster->name      = $body->name;
+        $monster->weight    = $body->weight;
+        $monster->size      = $body->size;
+        $monster->hp        = $body->hp;
+        $monster->attack    = $body->attack;
+        $monster->def       = $body->def;
+        $monster->agility   = $body->agility;
+        $monster->save();
+
+        return $res->withJson($body);
+    }
 }
