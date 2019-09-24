56c39e011dfbb7b1f8fcbd6a36ab264791a97cdc
ctrlouis <louis.bty57@gmail.com>
2019-09-24 07:54:55 +0000

    update Figth log model

--- src/models/FightLog.php
+++ src/models/FightLog.php
@@ -1,20 +1,18 @@
-<?php
+<?php namespace ratzslayer3\models;
 
-namespace ratzslayer3\models;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-use Illuminate/Database/Eloquent/SoftDeletes;
 
-class FightLog extends \Illuminate\Database\Eloquent\Model
-{
-  use SoftDeletes;
+class FightLog extends \Illuminate\Database\Eloquent\Model {
+
+    use SoftDeletes;
 
     protected $table = 'fights_log';
     protected $primaryKey = 'id';
+    
     public $timestamps = true;
 
     public function fightlog() {
-      return $this->belongsTo('ratzslayer3\models\Fight' , 'id_fight'); //L'id fight est pas forcément nécessaire
+        return $this->belongsTo('ratzslayer3\models\Fight' , 'id_fight'); //L'id fight est pas forcément nécessaire
     }
 }
-
-?>
