b969e675f744edabed222ceb9d385ae85240022f
ctrlouis <louis.bty57@gmail.com>
2019-09-24 07:56:19 +0000

    update Character model

--- src/models/Character.php
+++ src/models/Character.php
@@ -1,16 +1,14 @@
-<?php
-
-namespace ratzslayer3\models;
+<?php namespace ratzslayer3\models;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Character extends \Illuminate\Database\Eloquent\Model
-{
 
-  use SoftDeletes;
+class Character extends \Illuminate\Database\Eloquent\Model {
+
+    use SoftDeletes;
 
     protected $table = 'characters';
     protected $primaryKey = 'id';
+
     public $timestamps = true;
 }
-?>
