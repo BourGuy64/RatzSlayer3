a5ac26a87c95a45b4bd7fb5209e644eb34e19c0c
ctrlouis <louis.bty57@gmail.com>
2019-09-24 18:48:07 +0000

    import and init monster

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 "use strict";
 
-import * as Characters from './characters.js';
+import * as Characters  from './characters.js';
+import * as Monsters    from './monsters.js';
 
 
 function start() {
     Characters.init();
+    Monsters.init();
 }
 
 $(document).ready( function () {
