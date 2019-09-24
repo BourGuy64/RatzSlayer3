cafa87a83818c71d2eeca3db796cd15503578f75
ctrlouis <louis.bty57@gmail.com>
2019-09-24 07:54:29 +0000

    update Monster model

--- src/models/Monster.php
+++ src/models/Monster.php
@@ -1,15 +1,14 @@
-<?php
+<?php namespace ratzslayer3\models;
 
-namespace ratzslayer3\models;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-use Illuminate/Database/Eloquent/SoftDeletes;
 
-class Monster extends \Illuminate\Database\Eloquent\Model
-{
-  use SoftDeletes;
+class Monster extends \Illuminate\Database\Eloquent\Model {
+
+    use SoftDeletes;
 
     protected $table = 'monsters';
     protected $primaryKey = 'id';
+
     public $timestamps = true;
 }
-?>
