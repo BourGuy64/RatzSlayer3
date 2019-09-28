a2847566f5ff86c0822438a374cb169d16f5e696
ctrlouis <louis.bty57@gmail.com>
2019-09-28 22:39:56 +0000

    add file input and controller side image upload

--- src/controllers/MonstersController.php
+++ src/controllers/MonstersController.php
@@ -17,24 +17,69 @@ class MonstersController extends SuperController {
     }
 
     public function create(Request $req, Response $res, array $args) {
-        // $body = json_decode($req->getBody());
-        //
-        // $monster = MST::where('name', 'like', $body->name)->first();
-        // if ($monster) {
-        //     return $res->withStatus(400);
-        // }
-        //
-        // $monster = new MST;
-        // $monster->name      = $body->name;
-        // $monster->weight    = $body->weight;
-        // $monster->size      = $body->size;
-        // $monster->hp        = $body->hp;
-        // $monster->attack    = $body->attack;
-        // $monster->def       = $body->def;
-        // $monster->agility   = $body->agility;
-        // $monster->save();
 
-        // return $res->withJson($body);
-        return $res->withJson($_FILE[0]['name']);
+        $monster = MST::where('name', 'like', $_POST['name'])->first();
+        if ($monster) {
+            return $res->withStatus(400);
+        }
+
+        // image upload
+        $fileInfo = pathinfo($_FILES['img']['name']);
+        $extension = $fileInfo['extension']; // get the extension of the file
+        $fileName = $_POST['name'] . '.' . $extension;
+        $target = 'src/img/fighters/' . $fileName; // better link maybe ?
+        move_uploaded_file($_FILES['img']['tmp_name'], $target);
+
+        $monster = new MST;
+        $monster->name      = $_POST['name'];
+        $monster->weight    = $_POST['weight'];
+        $monster->size      = $_POST['size'];
+        $monster->hp        = $_POST['hp'];
+        $monster->attack    = $_POST['attack'];
+        $monster->def       = $_POST['def'];
+        $monster->agility   = $_POST['agility'];
+        $monster->picture   = $fileName;
+        $monster->save();
+
+        return $res->withJson($monster);
     }
+
+    // public function imageUpload($item)
+	// {
+	// 	if ($this->imageVerify($_FILES)) {
+	// 		if ($item->img) unlink($item->img);
+	// 		if (!is_dir('img/')) mkdir('img/');
+	// 		$fileName = "img/$item->name-icone.png";
+	// 		$resultat = move_uploaded_file($_FILES['icone']['tmp_name'], $fileName);
+	// 		// if ($resultat) echo "Transfert réussi";
+	// 		// $item->img = $fileName;
+	// 		// $item->save();
+	// 	}
+	// }
+    //
+	// public static function imageDelete($item)
+	// {
+	// 	if ($item->img) unlink($item->img);
+	// 	$item->img = NULL;
+	// 	$item->save();
+	// }
+    //
+	// public static function imageVerify($file)
+	// {
+	// 	if ($_FILES['img']['error'] > 0) {
+	// 		return false;
+	// 	}
+    //
+	// 	if ($_FILES['img']['size'] > 10000000) {
+	// 		return false;
+	// 	}
+    //
+	// 	$extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
+    //
+	// 	$extension_upload = strtolower(  substr(  strrchr($_FILES['img']['name'], '.')  ,1)  );
+	// 	if ( !in_array($extension_upload, $extensions_valides) ) {
+	// 		return false;
+	// 	}
+	// 	return true;
+	// }
 }
