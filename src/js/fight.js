5cec4724d00706716bcca9f78fe2d1dad337613f
Victor Bour <victor.bour.bowden@gmail.com>
2019-10-01 16:51:31 +0200

    Init fighters selection for fight

---
+++ src/js/fight.js
@@ -0,0 +1,39 @@
+"use strict"
+
+function selectChar(e){
+  $('.select-char').each(function(i){
+    $(this).removeClass('is-selected');
+  });
+  $(this).addClass('is-selected');
+  fightReady();
+}
+
+function selectMonster(e){
+  $('.select-monster').each(function(i){
+    $(this).removeClass('is-selected');
+  });
+  $(this).addClass('is-selected');
+  fightReady();
+}
+
+function fightReady(){
+  let ready = 0;
+  $('.select-monster').each(function(i){
+    if($(this).hasClass('is-selected') == 1){
+      ready++;
+    }
+  });
+  $('.select-char').each(function(i){
+    if($(this).hasClass('is-selected') == 1){
+      ready++;
+    }
+  });
+  if(ready === 2){
+    alert("Fight Ready !");
+  }
+}
+
+export function init() {
+    $('.select-char').on('click', selectChar);
+    $('.select-monster').on('click', selectMonster);
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 "use strict";
 
 import * as Fighters  from './fighters.js';
+import * as Fight from './fight.js';
 
 
 function start() {
     Fighters.init();
+    Fight.init();
 }
 
 $(document).ready( function () {
