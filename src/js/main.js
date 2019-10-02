8552ea037c7e425d7b5a6f349b0ef0941cc1bf42
ctrlouis <louis.bty57@gmail.com>
2019-10-02 00:59:56 +0000

    swag and useless code

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
 "use strict";
 
-import * as Fighters  from './fighters.js';
-import * as Fight from './fight.js';
+import * as Fighters    from './fighters.js';
+import * as Fight       from './fight.js';
 import * as Users       from './users.js';
+import * as Ui          from './ui.js';
 
 
 function start() {
     Fighters.init();
     Fight.init();
     Users.init();
+    Ui.init();
 }
 
 $(document).ready( function () {
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,1 +1,14 @@
 "use strict";
+
+function iconeSpin() {
+    $(this).addClass('fa-spin');
+}
+
+function iconeSpinStop() {
+    $(this).removeClass('fa-spin');
+}
+
+export function init() {
+    $('.fa-cog').on('mouseover', iconeSpin);
+    $('.fa-cog').on('mouseout', iconeSpinStop);
+}
