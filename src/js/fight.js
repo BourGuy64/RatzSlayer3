3496189a8bfc40426889275e1bc3aeea37e74853
ctrlouis <louis.bty57@gmail.com>
2019-10-24 08:47:02 +0200

    add request to get fight info

--- src/js/fight.js
+++ src/js/fight.js
@@ -1,6 +1,7 @@
 "use strict"
 
 import * as Conf from './conf.js';
+import Cookies from './../framework/js-cookie/js-cookie.js';
 
 function selectChar(e){
   $('.select-char').each(function(i){
@@ -45,6 +46,36 @@ function cancel(){
   });
 }
 
+function displayRound($logs) {
+    console.log($logs);
+
+}
+
+function getLastRound(fightId, fighterId, fighterType) {
+
+    const requestUrl = Conf.url.api + "/fightlog/last/" + fightId + "/" + fighterType + "/" + fighterId;
+
+    $.ajax({
+        type        : 'GET',
+        url         : requestUrl,
+        success     : (response, xhr) => {
+            // do something here
+            console.log(response); // DEV Return what twig generate but in console...
+            if (fighterType == 'c') {
+                $('.fight-log.character').prepend(response);
+            } else if (fighterType == 'm') {
+                $('.fight-log.monster').prepend(response);
+            }
+        },
+        error       : (xhr) => {
+            // do something for alert user
+            console.log("status =" + xhr.status); // DEV
+        },
+        complete    : () => {
+        }
+    });
+}
+
 function start(e) {
     e.preventDefault();
 
@@ -70,9 +101,49 @@ function start(e) {
     });
 }
 
+function nextRound(e) {
+
+    e.target.disabled = true;
+
+    const type = "POST";
+    const requestUrl = Conf.url.api + "/fightlog";
+
+    const fightId = Cookies.get('fight');
+    const char = $('.select-char').attr('data-id');
+    const monster = $('.select-monster').attr('data-id');
+    const formData = new FormData();
+    formData.append("fightId", fightId);
+    formData.append("char", char);
+    formData.append("monster", monster);
+
+    $.ajax({
+        type        : type,
+        url         : requestUrl,
+        timeout     : 5000,
+        header      : {},
+        data        : formData,
+        processData : false,
+        contentType : false,
+        success     : (response, xhr) => {
+            // do something here
+            getLastRound(fightId, char, 'c');
+            getLastRound(fightId, monster, 'm');
+        },
+        error       : (xhr) => {
+            // do something for alert user
+            console.log("status =" + xhr.status); // DEV
+            console.log(xhr);
+        },
+        complete    : () => {
+            e.target.disabled = false;
+        }
+    });
+}
+
 export function init() {
     $('.select-char').on('click', selectChar);
     $('.select-monster').on('click', selectMonster);
     $('.cancel-fight').on('click', cancel);
     $('.start-fight').on('click', start);
+    $('#next').on('click', nextRound);
 }
