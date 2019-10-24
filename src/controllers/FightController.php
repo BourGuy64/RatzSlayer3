977adfc2f3aa63ea93f6fd523802f0ac513f0cc0
ctrlouis <louis.bty57@gmail.com>
2019-10-24 08:46:49 +0200

    split fight function

--- src/controllers/FightController.php
+++ src/controllers/FightController.php
@@ -8,72 +8,57 @@ use ratzslayer3\models\FightLog                 as FGL;
 use ratzslayer3\models\Fight                    as FGT;
 use ratzslayer3\controllers\FightLogController  as FGLC;
 
+
 class FightController extends SuperController{
+
   public function get(Request $req, Response $res, array $args) {
-      if(isset($_COOKIE['CurrentFight'])){
-        $fightId = $_COOKIE['CurrentFight'];
-        $fight = FGT::where('id', $fightId)->first();
-        $character = CHR::where('id', $fight->id_characters)->first();
-        $monster = MST::where('id', $fight->id_monsters)->first();
-        $logChar = FGL::where('id_fights', $fightId)->where('fighter_type', 'c')->orderBy('id', 'asc')->get();
-        $logMonster = FGL::where('id_fights', $fightId)->where('fighter_type', 'm')->orderBy('id', 'asc')->get();
-        $winner = $this->winner($fightId);
-        return $this->views->render($res, 'fighting.html.twig', ['title' => 'Fight !', 'dir' =>  $this->dir, 'character' => $character, 'monster' => $monster, 'logChar' => $logChar, 'logMonster' => $logMonster, 'winner' => $winner, 'admin' => $_SESSION['admin']]);
-      }
-      else{
-        $characters = CHR::all();
-        $monsters = MST::all();
-        return $this->views->render($res, 'fight.html.twig', ['title' => 'Choose your fighter', 'dir' =>  $this->dir, 'characters' => $characters, 'monsters' => $monsters, 'admin' => $_SESSION['admin']]);
+      if (isset($_COOKIE['CurrentFight'])) {
+          $fightId = $_COOKIE['CurrentFight'];
+          $fight = FGT::where('id', $fightId)->first();
+          $character = CHR::where('id', $fight->id_characters)->first();
+          $monster = MST::where('id', $fight->id_monsters)->first();
+          $logChar = FGL::where('id_fights', $fightId)->where('fighter_type', 'c')->orderBy('id', 'asc')->get();
+          $logMonster = FGL::where('id_fights', $fightId)->where('fighter_type', 'm')->orderBy('id', 'asc')->get();
+          $winner = $this->winner($fightId);
+          return $this->views->render($res, 'fighting.html.twig', ['title' => 'Fight !', 'dir' =>  $this->dir, 'character' => $character, 'monster' => $monster, 'logChar' => $logChar, 'logMonster' => $logMonster, 'winner' => $winner, 'admin' => $_SESSION['admin']]);
+          // return $this->views->render($res, 'fighting.html.twig', ['title' => 'Fight !', 'dir' =>  $this->dir, 'character' => $character, 'monster' => $monster, 'admin' => $_SESSION['admin']]);
+      } else {
+          $characters = CHR::all();
+          $monsters = MST::all();
+          return $this->views->render($res, 'fight.html.twig', ['title' => 'Choose your fighter', 'dir' =>  $this->dir, 'characters' => $characters, 'monsters' => $monsters, 'admin' => $_SESSION['admin']]);
       }
   }
 
   //Do whole fight
-  public function fight(Request $req, Response $res, array $args){
-    $character = CHR::where('id', $_POST['char'])->first();
-    $monster = MST::where('id', $_POST['monster'])->first();
-    if(!($monster && $character)){
-      return $res->withStatus(400);
-    }
-    $fight = new FGT;
-    $fight->id_characters = $character->id;
-    $fight->id_monsters = $monster->id;
-    $fight->winner = 0;
-    $fight->save();
-    $round = 1;
-    //Fighter with more agility attack first (here is the MONSTER)
-    if($monster->agility > $character->agility){
-      FGLC::roundZero($monster, $character, $fight->id);
-      while(!FGLC::fighterIsDying($fight->id)){
-        FGLC::doAttack($monster, $character, $fight->id, $round);
-        if(!FGLC::fighterIsDying($fight->id)){
-          FGLC::doAttack($character, $monster, $fight->id, $round);
-        }
-        $round++;
+  public function fight(Request $req, Response $res, array $args) {
+      $character = CHR::find($_POST['char']);
+      $monster = MST::find($_POST['monster']);
+
+      if(!($monster && $character)){
+          return $res->withStatus(400);
       }
-    }
-    //If CHARACTER have more agility, he attack first
-    else{
-      FGLC::roundZero($character, $monster, $fight->id);
-      while(!FGLC::fighterIsDying($fight->id)){
-        FGLC::doAttack($character, $monster, $fight->id, $round);
-        if(!FGLC::fighterIsDying($fight->id)){
-          FGLC::doAttack($monster, $character, $fight->id, $round);
-        }
-        $round++;
+
+      $fight = new FGT;
+      $fight->id_characters = $character->id;
+      $fight->id_monsters = $monster->id;
+      $fight->winner = 0;
+      $fight->save();
+
+      if ($monster->agility > $character->agility) {
+          FGLC::roundZero($monster, $character, $fight->id);
+      } else {
+          FGLC::roundZero($character, $monster, $fight->id);
       }
-    }
-    $winner = $this->winner($fight->id);
-    $fight->winner = $winner;
-    $fight->save();
-    $logChar = FGL::where('id_fights', $fight->id)->where('fighter_type', 'c')->orderBy('id', 'asc')->get();
-    $logMonster = FGL::where('id_fights', $fight->id)->where('fighter_type', 'm')->orderBy('id', 'asc')->get();
-    setcookie("CurrentFight", $fight->id, time() + (10 * 365 * 24 * 60 * 60));
-    return $this->views->render($res, 'fighting.html.twig', ['title' => 'Fight !', 'dir' =>  $this->dir, 'character' => $character, 'monster' => $monster, 'logChar' => $logChar, 'logMonster' => $logMonster, 'winner' => $winner, 'admin' => $_SESSION['admin']]);
+
+      setcookie("CurrentFight", $fight->id, time() + (10 * 365 * 24 * 60 * 60));
+      setcookie("fight", $fight->id, time() + (10 * 365 * 24 * 60 * 60));
+
+      return $this->views->render($res, 'fighting.html.twig', ['title' => 'Fight !', 'dir' =>  $this->dir, 'character' => $character, 'monster' => $monster, 'admin' => $_SESSION['admin']]);
   }
 
   //Return fighter type of the winner
-  public function winner($fightId){
-    $winner = FGL::where('id_fights', $fightId)->where('hp', '>', '0')->orderBy('id', 'desc')->first();
-    return $winner->fighter_type;
+  public function winner($fightId) {
+      $winner = FGL::where('id_fights', $fightId)->where('hp', '>', '0')->orderBy('id', 'desc')->first();
+      return $winner->fighter_type;
   }
 }
--- src/controllers/FightLogController.php
+++ src/controllers/FightLogController.php
@@ -9,6 +9,82 @@ use ratzslayer3\models\Fight                    as FGT;
 
 class FightLogController extends SuperController{
 
+    public function get(Request $req, Response $res, array $args) {
+        $logs = FGL::find($args['id']);
+    }
+
+    public static function getRound($fight, $round) {
+        $lastRound = FGL::where('id_fights', $fight)
+            ->where('round', $round)
+            ->get();
+
+        return $lastRound;
+    }
+
+    public function getLast(Request $req, Response $res, array $args) {
+
+        $fight = FGT::find($args['fightId']);
+        if ($args['fighterType'] == 'c') {
+            $fighter = CHR::find($args['fighterId']);
+        } else if ($args['fighterType'] == 'm') {
+            $fighter = MST::find($args['fighterId']);
+        }
+
+        if (!$fighter) {
+            return $res->withStatus(400);
+        }
+
+        $log = FGL::orderBy('round', 'desc')
+            ->where('fighter_type', $args['fighterType'])
+            ->where('id_fights', $fight->id)
+            ->first();
+
+        return $this->views->render($res, 'fightlog.html.twig', ['dir' =>  $this->dir, 'fighter' => $fighter, 'log' => $log ]);
+    }
+
+    public function nextRound(Request $req, Response $res, array $args) {
+
+        $fight = FGT::find($_POST['fightId']);
+        $character = CHR::find($_POST['char']);
+        $monster = MST::find($_POST['monster']);
+        if(!($monster && $character)){
+            return $res->withStatus(400);
+        }
+
+        $lastRound = FGL::orderBy('round', 'desc')
+            ->where('id_fights', $fight->id)
+            ->first(); // gets the whole row
+        $currentRound = $lastRound->round + 1;
+
+
+        if ($monster->agility > $character->agility) {
+            $this->doAttack($character, $monster, $fight->id, $currentRound);
+            if (!$this->fighterIsDying($fight->id)) {
+                $this->doAttack($monster, $character, $fight->id, $currentRound);
+            }
+        } else {
+            $this->doAttack($monster, $character, $fight->id, $currentRound);
+            if (!$this->fighterIsDying($fight->id)) {
+                $this->doAttack($character, $monster, $fight->id, $currentRound);
+            }
+        }
+
+        // return $res->withJson(SELF::getRound($fight->id, $currentRound));
+
+        $logChar = FGL::where('id_fights', $fight->id)->where('fighter_type', 'c')->orderBy('id', 'asc')->get();
+        $logMonster = FGL::where('id_fights', $fight->id)->where('fighter_type', 'm')->orderBy('id', 'asc')->get();
+        return $this->views->render($res, 'fightlog.html.twig', ['dir' =>  $this->dir, 'character' => $character, 'monster' => $monster, 'logChar' => $logChar, 'logMonster' => $logMonster, ]);
+
+
+        // $winner = $this->winner($fight->id);
+        // $fight->winner = $winner;
+        // $fight->save();
+        // $logChar = FGL::where('id_fights', $fight->id)->where('fighter_type', 'c')->orderBy('id', 'asc')->get();
+        // $logMonster = FGL::where('id_fights', $fight->id)->where('fighter_type', 'm')->orderBy('id', 'asc')->get();
+        // setcookie("CurrentFight", $fight->id, time() + (10 * 365 * 24 * 60 * 60));
+        // return $this->views->render($res, 'fighting.html.twig', ['title' => 'Fight !', 'dir' =>  $this->dir, 'character' => $character, 'monster' => $monster, 'logChar' => $logChar, 'logMonster' => $logMonster, 'winner' => $winner, 'admin' => $_SESSION['admin']]);
+    }
+
   public static function doAttack($fighter, $target, $fightId, $round){
     //Create new fight log
     $fightlog = new FGL;
@@ -16,12 +92,21 @@ class FightLogController extends SuperController{
     $fightlog->id_fighter = $target->id;
     $fightlog->round = $round;
     $fightlog->fighter_type = $target->type;
+
     //Get hp of target from last round
-    $lastRound = FGL::where('round', $round-1)->where('id_fighter', $target->id)->where('fighter_type', $target->type)->orderBy('id', 'desc')->first();
+    $lastRound = FGL::where('round', $round-1)
+        ->where('id_fighter', $target->id)
+        ->where('fighter_type', $target->type)
+        ->orderBy('id', 'desc')
+        ->first();
+
+    $fightlog->damage = $fighter->attack;
     $leftLife = $lastRound->hp - $fighter->attack;
-    if($leftLife < 0){
-      $leftLife = 0;
+
+    if ($leftLife < 0) {
+        $leftLife = 0;
     }
+
     $fightlog->hp = $leftLife;
     $fightlog->save();
   }
